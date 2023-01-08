cbe66aa986a8e7a05c3d80857260e4e628f6ec51
Wh4I3 <siegfried.dahlberg@gmail.com>
2023-01-08 18:15:08 +0100

    Fixed KillAll() and renamed it to Butcher() And added a Tp and GetPosition Command

--- Commands.cs
+++ Commands.cs
@@ -8,7 +8,6 @@ namespace OutpathConsole
 {
     public class Commands
     {
-
         public WeaponInfo Weapon;
         public string Help(string input)
         {
@@ -19,15 +18,16 @@ namespace OutpathConsole
                 if (input == "Help") return "Help(<Command>): Sends a list of commands if <Command> is empty. Otherwise it tells what a certain command does.";
                 else if (input == "HelloWorld") return "HelloWorld(): Writes \"Hello World!\" to the Console.";
                 else if (input == "Echo") return "Echo(<Message>): Writes the <Message> to the Console";
-                else if (input == "GiveCredits") return "GiveCredits(<Quantity>): Gives you <Quantity> Credits";
-                else if (input == "GiveItem") return "GiveItem(<Item Name>, <Quantity>): Gives you <Quantity> of <Item Name>.";
-                else if (input == "GiveAllItems") return "GiveAllItems(<Quantity>): Gives you <Quantity> of all items.";
+                else if (input == "GiveItem") return "GiveItem(<Item Name>, <Amount>): Gives you <Amount> of <Item Name>.";
+                else if (input == "GiveAllItems") return "GiveAllItems(<Amount>): Gives you <Amount> of all items.";
                 else if (input == "GetItem") return "GetItem(<ItemID>): Tells you the name of the item with <ItemID> as the ItemID.";
                 else if (input == "GetAllItems") return "GetAllItems(): Tells you the ItemID and name of every item.";
                 else if (input == "Respawn") return "Respawn(): Respawns you.";
                 else if (input == "AddCredits") return "AddCredits(<Amount>): Gives you the games currency.";
                 else if (input == "CreateWeapon") return "CreateWeapon(): Creates a weapon. Right click to shoot";
-                else if (input == "KillAll") return "KillAll(): Kills all animals";
+                else if (input == "Butcher") return "Butcher(): Kills all animals";
+                else if (input == "Tp") return "Tp(<x>, <y>, <z>): Teleports you to the position specified.";
+                else if (input == "GetPosition") return "GetPosition(): Gets your current position.";
                 else return "No such command";
             }
             else
@@ -36,7 +36,6 @@ namespace OutpathConsole
                 returnString += "\nHelp";
                 returnString += "\nHelloWorld";
                 returnString += "\nEcho";
-                returnString += "\nGiveCredits";
                 returnString += "\nGiveItem";
                 returnString += "\nGiveAllItems";
                 returnString += "\nGetItem";
@@ -44,7 +43,9 @@ namespace OutpathConsole
                 returnString += "\nRespawn";
                 returnString += "\nAddCredits";
                 returnString += "\nCreateWeapon";
-                returnString += "\nKillAll";
+                returnString += "\nButcher";
+                returnString += "\nTp";
+                returnString += "\nGetPosition";
                 returnString += "\n\n(You can see what each command does with /Help(<Command>).";
             }
 
@@ -59,13 +60,6 @@ namespace OutpathConsole
         {
             Melon<Mod>.Logger.Msg(input);
             return input;
-        }
-        public string GiveCredits(string input)
-        {
-            int quantity = int.Parse(input);
-            PlayerGarden.instance.AddCredits(quantity);
-            Melon<Mod>.Logger.Msg("Gave " + quantity + " [Credits]");
-            return "Gave " + quantity + " [Credits]";
         }
         public string GiveItem(string input)
         {
@@ -127,26 +121,28 @@ namespace OutpathConsole
         }
         public string Respawn(string input)
         {
-            string returnString = "";
+            string returnString;
 
             PlayerGarden.instance.GetDrown();
             returnString = "Respawned!";
 
+            Melon<Mod>.Logger.Msg(returnString);
             return returnString;
         }
         public string AddCredits(string input)
         {
-            string returnString = "";
+            string returnString;
 
             PlayerGarden.instance.AddCredits(int.Parse(input));
             returnString = "Gave " + input + " Credits.";
 
+            Melon<Mod>.Logger.Msg(returnString);
             return returnString;
         }
 
         public string CreateWeapon(string input)
         {
-            string returnString = "Created a weapon??";
+            string returnString;
 
             int WeaponIndex = int.Parse(input);
             WeaponInfo[] _Weapon = Resources.FindObjectsOfTypeAll<WeaponInfo>();
@@ -164,22 +160,49 @@ namespace OutpathConsole
             PlayerWeaponManager.instance.SetupWeapon(Weapon);
             returnString = "Created Weapon: " + Weapon.name;
 
+            Melon<Mod>.Logger.Msg(returnString);
             return returnString;
         }
-        public string KillAll(string input)
+        public string Butcher(string input)
         {
-            string returnString = "Killing all animals";
+            string returnString;
+            EnemyHealth[] Enemies = Resources.FindObjectsOfTypeAll<EnemyHealth>();
 
-            EnemyHealth[] Enemys = Resources.FindObjectsOfTypeAll<EnemyHealth>();
-
-            foreach(var Animal in Enemys)
+            foreach(var Animal in Enemies)
             {
-                Animal.health = 0;
-                Animal.DamageEnemy();
+                Animal.DestroyEnemy();
             }
 
             returnString = "Killed all animals/creatures.";
 
+            Melon<Mod>.Logger.Msg(returnString);
+            return returnString;
+        }
+        public string Tp(string input)
+        {
+            string returnString;
+            string[] parts = input.Split(new char[] { ',' }, 3);
+
+            float x = float.Parse(parts[0]);
+            float y = float.Parse(parts[1]);
+            float z = float.Parse(parts[2]);
+
+            PlayerGarden.instance.transform.position = new Vector3(x, y, z);
+
+            returnString = "Teleported player to: [x: " + x + "], [y: " + y + "], [z: " + z + "]";
+
+            Melon<Mod>.Logger.Msg(returnString);
+            return returnString;
+        }
+        public string GetPosition(string input)
+        {
+            string returnString;
+
+            Vector3 position = PlayerGarden.instance.transform.position;
+
+            returnString = "Your position is: [x: " + position.x + "], [y: " + position.y + "], [z: " + position.z + "]";
+
+            Melon<Mod>.Logger.Msg(returnString);
             return returnString;
         }
     }
